adf09d23d5043a37c2c42cacbfa7c3371c72d171
Todd Schluechtermann <dyson_swarm@outlook.com>
2024-06-22 13:21:09 -0500

    Testing

--- app/auth.ts
+++ app/auth.ts
@@ -1,8 +1,6 @@
+import prisma from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
-
-const prisma = new PrismaClient();
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
