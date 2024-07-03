0f06e7108b764b7de970d0b19191d7e423bb5a4d
Todd Schluechtermann <dyson_swarm@outlook.com>
2024-07-03 14:37:48 -0500

    github

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,15 @@
 import prisma from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import Resend from "next-auth/providers/resend";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
+    Google,
+    Github,
     Resend({
       apiKey: process.env.AUTH_RESEND_KEY,
       from: "Schluechtermann <no-reply@schluechtermann.com>",
