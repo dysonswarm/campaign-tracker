f23d227b8c1f18d6ce72467f318636463672c1ec
Todd Schluechtermann <dyson_swarm@outlook.com>
2024-07-16 20:30:10 -0500

    Update connection

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
 	const neon = new Pool({
-		connectionString: process.env.POSTGRES_PRISMA_URL,
+		connectionString: process.env.DATABASE_URL,
 	});
 	const adapter = new PrismaNeon(neon);
 	return new PrismaClient({ adapter });
