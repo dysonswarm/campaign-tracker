335686d1576339c0c8d72a3be998926531a006b0
Todd Schluechtermann <dyson_swarm@outlook.com>
2024-09-07 22:17:00 +0000

    Container updates

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 pnpm install
 
-read -p "Enter Git Name: " name
+FILE=./.env
+if test -f "$FILE"; then
+    echo ".env file already created"
+else
+    read -p "OPENAI_API_KEY: " openapikey
+    read -p "LANGCHAIN_API_KEY: " langchainapikey
 
-read -p "Enter Git Email: " email
-git config --global user.name "$name"
-git config --global user.email "$email"
+    touch ./.env
+    echo "DATABASE_URL=postgresql://localhost:5432/campaign-tracker?sslmode=require
+    OPENAI_API_KEY=$openapikey
+    LANGCHAIN_TRACING_V2=true
+    LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
+    LANGCHAIN_API_KEY=$langchainapikey
+    LANGCHAIN_PROJECT=campaign-tracker-$(cut -d @ -f 1 <<< "$email")" >> ./.env
+fi
 
-read -p "OPENAI_API_KEY: " openapikey
-read -p "LANGCHAIN_API_KEY: " langchainapikey
-
-touch ./.env
-echo "DATABASE_URL=postgresql://localhost:5432/campaign-tracker?sslmode=require
-OPENAI_API_KEY=$openapikey
-LANGCHAIN_TRACING_V2=true
-LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
-LANGCHAIN_API_KEY=$langchainapikey
-LANGCHAIN_PROJECT=campaign-tracker-$(cut -d @ -f 1 <<< "$email")" >> ./.env
