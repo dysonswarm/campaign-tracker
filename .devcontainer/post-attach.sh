ac7e0e682d12306390527f3794ce703b8835157d
Todd Schluechtermann <dyson_swarm@outlook.com>
2024-09-07 19:57:26 +0000

    Update post attach script

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -6,4 +6,14 @@ read -p "Enter Git Name: " name
 read -p "Enter Git Email: " email
 git config --global user.name "$name"
 git config --global user.email "$email"
-git config --list
+
+read -p "OPENAI_API_KEY: " openapikey
+read -p "LANGCHAIN_API_KEY: " langchainapikey
+
+touch ./.env
+echo "DATABASE_URL=postgresql://localhost:5432/campaign-tracker?sslmode=require
+OPENAI_API_KEY=$openapikey
+LANGCHAIN_TRACING_V2=true
+LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
+LANGCHAIN_API_KEY=$langchainapikey
+LANGCHAIN_PROJECT=campaign-tracker-$(cut -d @ -f 1 <<< "$email")" >> ./.env
