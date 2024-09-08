38db661d2754c089845c5a03223207bc71db5e1f
Todd Schluechtermann <dyson_swarm@outlook.com>
2024-09-08 03:16:15 +0000

    cleanup

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 pnpm install
 
+pnpm setup
+
 FILE=./.env
-if test -f "$FILE"; then
+if [[ -f "$FILE" ]]; then
     echo ".env file already created"
 else
-    read -p "OPENAI_API_KEY: " openapikey
+    while [[ -z "$openapikey" ]]; do
+        read -p "OPENAI_API_KEY: " openapikey
+    done    
     read -p "LANGCHAIN_API_KEY: " langchainapikey
 
-    touch ./.env
-    echo "DATABASE_URL=postgresql://localhost:5432/campaign-tracker?sslmode=require
-    OPENAI_API_KEY=$openapikey
-    LANGCHAIN_TRACING_V2=true
-    LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
-    LANGCHAIN_API_KEY=$langchainapikey
-    LANGCHAIN_PROJECT=campaign-tracker-$(cut -d @ -f 1 <<< "$email")" >> ./.env
+    cat << EOF >> ./.env
+DATABASE_URL=postgresql://localhost:5432/campaign-tracker?sslmode=require
+OPENAI_API_KEY=$openapikey
+LANGCHAIN_TRACING_V2=true
+LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
+LANGCHAIN_API_KEY=$langchainapikey
+LANGCHAIN_PROJECT=campaign-tracker-$(cut -d @ -f 1 <<< "$email")
+EOF
 fi
 
